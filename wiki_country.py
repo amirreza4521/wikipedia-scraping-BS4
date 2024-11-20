48124c147e5bf77d225d4bde661b5718873c9a1e
amirreza4521 <amir.reza4521@gmail.com>
2024-11-20 21:05:52 +0330

    add table headers

--- wiki_country.py
+++ wiki_country.py
@@ -5,4 +5,14 @@ header={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 
 }
-response=requests.k
+url="https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+response=requests.get(url=url,headers=header)
+# print(response.reason)
+soup=BeautifulSoup(response.text,"html.parser")
+div_table=soup.select_one("div.mw-content-ltr.mw-parser-output")
+table=div_table.find("table",class_="wikitable")
+table_header=[]
+for row in table.find("tbody").find("tr").find_all("th"):
+    header=row.get_text().strip()
+    table_header.append(header)
+print(table_header)
